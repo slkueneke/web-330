9b82b63b0861b425a3741a42f425aff8b2f8e23c
Shannon Kueneke <slkueneke@gmail.com>
2025-10-28 16:48:42 -0700

    week 2 assignment

--- week-2/character-generator/script.js
+++ week-2/character-generator/script.js
@@ -3,23 +3,65 @@
   Chapter 1
   Programming Assignment
 
-  Author:
-  Date:
-  Filename:
+  Author: Shannon Kueneke
+  Date: October 25, 2025
+  Filename: script.js
 */
 
 "use strict";
 
+function enableCTA() {
+  document.getElementById("characterForm").onchange = function() {
+    if (!!document.getElementById("heroName").value &&
+        !!document.getElementById("heroGender").value &&
+        !!document.getElementById("heroClass").value
+    ) {
+      document.getElementById("generateHero").removeAttribute("disabled");
+    }
+  };
+}
+enableCTA();
+
+
 function createCharacter(name, gender, characterClass) {
-  // TODO: Implement this function
+  return {
+    getName: function () {
+      return name;
+    },
+    getGender: function () {
+      return gender;
+    },
+    getClass: function () {
+      return characterClass;
+    },
+  };
 }
 
 document.getElementById("generateHero").addEventListener("click", function(e) {
   e.preventDefault();
 
-  // TODO: Get form values
+  if (this.getAttribute("disabled")) {
+    return false;
+  }
 
-  // TODO: Create character
+  // Get form values
+  let name = document.getElementById("heroName").value;
+  let gender = document.getElementById("heroGender").value;
+  let char = document.getElementById("heroClass").value;
 
-  // TODO: Display character information
+  // Create character
+  let newCharacter = createCharacter(name, gender, char);
+
+  // Display character information
+  function addInfo() {
+    let charName = newCharacter.getName();
+    let charGender = newCharacter.getGender();
+    let charClass = newCharacter.getClass();
+
+    let welcomeMsg = '<p>WELCOME, ' + charGender.toUpperCase() + ' ' + charClass.toUpperCase() + ' ' + charName.toUpperCase() + '!</p><img src="images/' + charClass + '_' + charGender + '.png" alt="Hero Image" >';
+    document.getElementById("characterForm").classList.add("hide");
+    document.getElementById("characterOutput").innerHTML = welcomeMsg;
+
+  }
+  addInfo();
 });
