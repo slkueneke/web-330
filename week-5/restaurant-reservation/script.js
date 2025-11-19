323cd1cdfc66775877ac6748d815f822d3add980
Shannon Kueneke <slkueneke@gmail.com>
2025-11-18 16:01:36 -0800

    week-5 assignment

--- week-5/restaurant-reservation/script.js
+++ week-5/restaurant-reservation/script.js
@@ -3,24 +3,65 @@
   Chapter 2
   Programming Assignment
 
-  Author:
-  Date:
-  Filename:
+  Author: Shannon Kueneke
+  Date: November 17, 2025
+  Filename: script.js
 */
 
 // Create an in-memory object array for each table in the restaurant
-let tables = [
-  // Add your table objects here
+let tables= [
+  {
+    tableNumber: 1,
+    capacity: 4,
+    isReserved: false
+  },
+  {
+    tableNumber: 2,
+    capacity: 2,
+    isReserved: false
+  },
+  {
+    tableNumber: 3,
+    capacity: 6,
+    isReserved: false
+  }
 ];
 
 // Create a function reserveTable
 function reserveTable(tableNumber, callback, time) {
-  // Add your code here
+  // loop through tables array
+  for (let i = 0; i < tables.length; i++) {
+    //find obj for selectedTable number
+    if (tables[i].tableNumber == tableNumber) {
+      if (!tables[i].isReserved) {
+        //book table
+        tables[i].isReserved = true;
+
+        setTimeout(function() {
+          callback("success");
+        }, time);
+        break;
+      } else {
+        callback("fail");
+      }
+    }
+  }
 }
 
 // When the form is submitted, call the reserveTable function
 document
   .getElementById("reservationForm")
-  .addEventListener("submit", function (e) {
-    // Add your code here
+  .addEventListener("submit", function (e) { console.log('submit')
+    e.preventDefault();
+
+    function resMessaging(status) {
+      if (status === "success") {
+        document.getElementById("message").textContent = "Table has been successfully booked!";
+      } else if (status === "fail") {
+        document.getElementById('message').textContent = 'Error: Table is not available!';
+      }
+    }
+    // get selected table number from form
+    let selectedTable = document.getElementById("tableNumber").value;
+    reserveTable(selectedTable, resMessaging, 2000);
   });
